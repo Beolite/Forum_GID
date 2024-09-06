020f9f5e54445e76d4d81d53cd44be533673fec1
unknown <wwaterr313@gmail.com>
2024-09-06 21:12:03 +0700

    modified authentication in user route

--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -9,9 +9,9 @@ import { adminOnly, verifyUser } from "../middleware/AuthUser.js";
 
 const router = express.Router();
 
-router.get('/users',verifyUser,adminOnly, getUsers);
-router.get('/users/:id',verifyUser,adminOnly, getUserById);
-router.post('/users',verifyUser,adminOnly, createUser);
+router.get('/users',verifyUser, getUsers);
+router.get('/users/:id',verifyUser, getUserById);
+router.post('/users', createUser);
 router.patch('/users/:id',verifyUser,adminOnly, updateUser);
 router.delete('/users/:id',verifyUser,adminOnly, deleteUser);
 
